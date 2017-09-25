a9ba3c6fb5597b75b7022c6d8211915a1158eacc
Lana Smith <lanasmith.koi@gmail.com>
2017-09-25 15:41:25 -0400

    add gulp sass compiler

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+
+const sass = require('gulp-sass')
+
+
+gulp.task('sass', fuction (){
+  return gulp
+    .src('scss/**/*.scss')
+    .pipe(sass())
+
+    .pipe(gulp.dest('css'))
+})
